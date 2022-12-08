94a232bb0cf0c03975e15bcdee064fb30ca7db25
Eskin Nikita <ladeuxmaster@gmail.com>
2022-12-08 15:26:35 +0300

    Add message about empty list for events list

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -7,26 +7,33 @@
   >
     <v-list color="transparent">
       <v-list-item><h2>Events</h2></v-list-item>
-      <v-list-item v-for="(event, index) in eventsStore.events" :key="index">
-        <span class="events-list__item-id">id: {{ event.id }}</span>
-        <v-btn variant="text" icon @click="removeEvent(event.id)"
-          ><v-icon>mdi-close-circle-outline</v-icon></v-btn
-        >
-        <ul class="mb-1">
-          <li v-if="getValue('tasks', event.taskId, 'title')">
-            <strong>Task: </strong>
-            {{ getValue("tasks", event.taskId, "title") }}
-          </li>
-          <li v-if="getValue('dates', event.dateId, 'date')">
-            <strong>Date: </strong>{{ getValue("dates", event.dateId, "date") }}
-          </li>
-          <li v-if="getValue('statuses', event.statusId, 'status')">
-            <strong>Status:</strong>
-            {{ getValue("statuses", event.statusId, "status") }}
-          </li>
-        </ul>
-        <v-divider v-if="index < eventsStore.events.length - 1" :key="index" />
-      </v-list-item>
+      <template v-if="eventsStore.events.length">
+        <v-list-item v-for="(event, index) in eventsStore.events" :key="index">
+          <span class="events-list__item-id">id: {{ event.id }}</span>
+          <v-btn variant="text" icon @click="removeEvent(event.id)"
+            ><v-icon>mdi-close-circle-outline</v-icon></v-btn
+          >
+          <ul class="mb-1">
+            <li v-if="getValue('tasks', event.taskId, 'title')">
+              <strong>Task: </strong>
+              {{ getValue("tasks", event.taskId, "title") }}
+            </li>
+            <li v-if="getValue('dates', event.dateId, 'date')">
+              <strong>Date: </strong
+              >{{ getValue("dates", event.dateId, "date") }}
+            </li>
+            <li v-if="getValue('statuses', event.statusId, 'status')">
+              <strong>Status:</strong>
+              {{ getValue("statuses", event.statusId, "status") }}
+            </li>
+          </ul>
+          <v-divider
+            v-if="index < eventsStore.events.length - 1"
+            :key="index"
+          />
+        </v-list-item>
+      </template>
+      <div class="text-center mt-2 mb-2" v-else>Events list is empty</div>
     </v-list>
   </v-navigation-drawer>
 </template>
